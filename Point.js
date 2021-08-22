7f9eff09a19c127533dbffd5e91b94ba8c946d73
Snozm <89019256+Snozm@users.noreply.github.com>
2021-08-22 19:42:20 +0300

    Removed unneeded comments

--- Point.js
+++ Point.js
@@ -1,10 +1,8 @@
 module.exports = class Point {
-// Takes an x and y coordinate as parameters 
     constructor(x, y) {
             this.x=x
             this.y=y
     }
-    // Draws the point with the x and y coordinates as the center of the dot(if needed)
     draw() {
         context.fillRect(this.position.x - 5, this.position.y - 5, 10, 10);
     }
