3676a7c8b974ef0f448660bba2c75586c78850ad
Snozm <89019256+Snozm@users.noreply.github.com>
2021-08-22 19:31:54 +0300

    Added comment indentation

--- Point.js
+++ Point.js
@@ -1,12 +1,12 @@
 module.exports = class Point {
-//Takes an x and y coordinate as parameters 
+// Takes an x and y coordinate as parameters 
     constructor(x, y) {
             this.x=x
             this.y=y
     }
-    //draws the point with the x and y coordinates as the center of the dot(if needed)
+    // Draws the point with the x and y coordinates as the center of the dot(if needed)
     draw() {
         context.fillRect(this.position.x - 5, this.position.y - 5, 10, 10);
     }
 }
-//bottom text
+// bottom text
