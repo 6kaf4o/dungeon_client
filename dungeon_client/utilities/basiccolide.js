bb0e4640c49aabc8f734e892b9e478f76c4f0767
Kiproasdf <39674799+kipriyan@users.noreply.github.com>
2021-08-23 18:31:33 +0300

    arecolliding

---
+++ dungeon_client/utilities/basiccolide.js
@@ -0,0 +1,12 @@
+﻿function areColliding(Ax, Ay, Awidth, Aheight, Bx, By, Bwidth, Bheight) {
+    if (Bx <= Ax + Awidth) {
+        if (Ax <= Bx + Bwidth) {
+            if (By <= Ay + Aheight) {
+                if (Ay <= By + Bheight) {
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+};
