4c552886e206552c6c9cf468c52c8392633e6f7a
VVbonev <42972737+vvbonev@users.noreply.github.com>
2021-08-23 16:16:06 +0300

    Update Sheet.js

--- dungeon_client/utilities/Sheet.js
+++ dungeon_client/utilities/Sheet.js
@@ -8,6 +8,6 @@ module.exports = class Sheet {
         this.imageHeight = properties.height;
     }
     draw(positionInSheet, position, w, h) {
-        context.drawImage(this.sheet, positionInSheet.x * this.imageWidth / this.rows, positionInSheet.y * this.imageHeight / this.columns, this.imageWidth / this.rows, this.imageHeight / this.columns, position.x - w / 2, position.y - h / 2, w, h)
+        context.drawImage(this.sheet, positionInSheet.x * this.imageWidth / this.rows, positionInSheet.y * this.imageHeight / this.columns, this.imageWidth / this.rows, this.imageHeight / this.columns, position.x - w / 2, position.y - h / 2, w, h);
     }
 }
