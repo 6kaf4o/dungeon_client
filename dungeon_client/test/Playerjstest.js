ab994267827913ad550af60a121caefd142b7159
VVbonev <42972737+vvbonev@users.noreply.github.com>
2021-08-23 10:09:09 +0300

    Fixed PlayerJS,PlayerjstestJS,SheetJS and changed images's names to proper ones

--- dungeon_client/test/Playerjstest.js
+++ dungeon_client/test/Playerjstest.js
@@ -1,68 +1,55 @@
-var walls = []
-
-var intersections = []
+var walls = [];
+var intersections = [];
 
 walls.push(new Line(new Point(0, 0), new Point(800, 0)));
 walls.push(new Line(new Point(800, 600), new Point(800, 0)));
 walls.push(new Line(new Point(800, 600), new Point(0, 600)));
 walls.push(new Line(new Point(0, 0), new Point(0, 600, 1)));
 
-for(let i = 0 ; i < Math.random()*4+1 ; i ++){
-    walls.push (new Line(new Point(Math.random()*300 , Math.random()*300),new Point(Math.random()*300 , Math.random()*300)))
+for (let i = 0; i < Math.random() * 4 + 1; i++) {
+    walls.push(new Line(new Point(Math.random() * 300, Math.random() * 300), new Point(Math.random() * 300, Math.random() * 300)));
 }
 
-class Magic{
-    constructor(x,y,dx,dy){
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
-    }
-    draw(){
-        this.x += this.dx
-        this.y += this.dy
-        context.fillRect(this.x,this.y,20,20)
-    }
+var p_spritesheet = new Sheet('./testchar', { rows: 4, columns: 4, width: 1601, height: 2397 })
+var p = new Player(new Point(100, 100), 20, [], 0, );
+var lighting = new Lighting(walls);
+
+function update() {
+    intersections = lighting.drawLight(p.position);
+    p.update();
 }
 
-var p = new Player(new Point(100,100),20,[],0)
+function draw() {
 
-var lighting = new Lighting(walls)
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-function update (){
-    intersections = lighting.drawLight(p.position)
-    p.update()
-    console.log(lighting)
-}
-
-function draw(){
-
-    context.fillStyle = "black"
-    context.fillRect(0,0,canvas.width, canvas.height)
-    
-    context.fillStyle = "white"
-    context.beginPath()
-    for(let i = 1 ; i < intersections.length ; i ++){
-        context.lineTo(intersections[i].x,intersections[i].y)
+    context.fillStyle = "white";
+    context.beginPath();
+    for (let i = 1; i < intersections.length; i++) {
+        context.lineTo(intersections[i].x, intersections[i].y);
     }
-    context.fill()
-    lighting.drawLight(p.position)
+    context.fill();
 
-    context.strokeStyle = "red"
+    lighting.drawLight(p.position);
+
+    context.strokeStyle = "red";
     context.lineWidth = 3;
-    for(let i = 0 ; i < walls.length ; i ++){        
+
+    for (let i = 0; i < walls.length; i++) {
         context.beginPath()
-        context.lineTo(walls[i].begin.x,walls[i].begin.y)
-        context.lineTo(walls[i].end.x,walls[i].end.y)
-        context.stroke()
+        context.lineTo(walls[i].begin.x, walls[i].begin.y);
+        context.lineTo(walls[i].end.x, walls[i].end.y);
+        context.stroke();
     }
 
-    p.draw()
-    for(let i = 0 ; i < p.bullets ; i ++){
-        p.bullet[i].draw()
+    p.draw();
+
+    for (let i = 0; i < p.bullets; i++) {
+        p.bullet[i].draw();
     }
 }
 
-function mouseup(){
-    p.shoot()
+function mouseup() {
+    p.shoot();
 }
--- dungeon_client/utilities/Sheet.js
+++ dungeon_client/utilities/Sheet.js
@@ -1,10 +1,13 @@
-module.exports = class Sheet{
-    constructor(sheet, x, y, w, h){
+module.exports = class Sheet {
+    constructor(sheet, properties) {
         this.sheet = new Image();
-        this.sheet.src = 'images/' + sheet;
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
+        this.sheet.src = sheet;
+        this.rows = properties.rows;
+        this.columns = properties.columns;
+        this.imageWidth = properties.width;
+        this.imageHeight = properties.height;
+    }
+    draw(positionInSheet, position, w, h) {
+        context.drawImage(this.sheet, positionInSheet.x * this.imageWidth / this.rows, positionInSheet.y * this.imageHeight / this.columns, this.imageWidth / this.rows, this.imageHeight / this.columns, position.x - w / 2, position.y - h / 2, w, h)
     }
 }
