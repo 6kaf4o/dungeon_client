01c4996ef8f460483afa7fe79ce89ccaed7d290e
Snozm <89019256+Snozm@users.noreply.github.com>
2021-08-23 08:59:46 +0300

    Removed undefineds

--- dungeon_client/light/Lighting.js
+++ dungeon_client/light/Lighting.js
@@ -10,7 +10,7 @@ module.exports = class Lighting {
         // This for loop does all the checks for intersection at angle intervals of 0.002
         for(this.angle = 0; this.angle < Math.PI * 2; this.angle += 0.002) {
             // This is a single intersection that will go into the intersections array later(if it gets a value)
-            this.intersection = undefined;
+            this.intersection = false;
             // Minimum distance defines the maximum length our light rays can have
             this.minimumDistance = 10000;
             // Just a ray rotated around the starting point by an angle amount
@@ -19,8 +19,8 @@ module.exports = class Lighting {
             for(this.i = 0; this.i < this.walls.length; this.i ++) {
                 // Current intersection is a holding variable that has a sole purpose to hold the result of the intersection check
                 this.currentIntersection = Utility.wallRayIntersection(this.walls[this.i], this.rotatedRay);
-                // If a ray doesn't collide with a wall it is undefined
-                if(this.currentIntersection !== undefined) {
+                // If a ray doesn't collide with a wall their intersection is false
+                if(this.currentIntersection !== false) {
                     // We determine the distance of the light ray from the player to a wall
                     if(this.minimumDistance > distance(this.currentIntersection, start)) {
                         this.intersection = this.currentIntersection;
@@ -29,7 +29,7 @@ module.exports = class Lighting {
                 }
             }
             // We push an intersection of value into the intersections array
-            if(this.intersection != undefined) {
+            if(this.intersection != false) {
                 this.intersections.push(this.intersection);
             }
         }
