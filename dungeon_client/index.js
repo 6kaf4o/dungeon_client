eff9544be90e68900fcd6db2f88cb5a9bee61010
Atanas Dimitrov <nasko119@abv.bg>
2021-08-25 14:58:39 +0200

    Index.js update.

--- dungeon_client/index.js
+++ dungeon_client/index.js
@@ -1,12 +1,12 @@
 const Player = require('/player/Player.js');
 const Inventory = require('/player/Inventory.js');
-const Weapon = require('/weapons/Weapon.js');
 const Lighting = require('/light/Lighting.js');
 const Gamestate = require('/framework/State.js');
 const Basegame = require('/framework/Framework.js');
 const Geometry = require('/utilities/Geometry.js');
 const Point = Geometry.Point;
 const Line = Geometry.Line;
+const Weapon = require('/weapons/Weapon.js');
 
 class Game extends Basegame {
     constructor() {
@@ -20,12 +20,12 @@ class Game extends Basegame {
         this.walls.push(new Line(new Point(800, 600), new Point(0, 600)));
         this.walls.push(new Line(new Point(0, 0), new Point(0, 600, 1)));
 
-        for (let i = 0; i < Math.random() * 4 + 1; i++) {
-            this.walls.push(new Line(new Point(Math.random() * 300, Math.random() * 300), new Point(Math.random() * 300, Math.random() * 300)));
+        for(let i = 0 ; i < Math.random()*4+1 ; i ++){
+            this.walls.push (new Line(new Point(Math.random()*300 , Math.random()*300),new Point(Math.random()*300 , Math.random()*300)));
         }
 
-        this.player = new Player(new Point(100, 100), 20, new Inventory(10), 0);
-        this.player.inventory.content[0] = new Weapon();
+        this.player = new Player(new Point(100,100), 20, new Inventory(10), 0);
+        this.player.inventory.equipItem(new Weapon.BasicGun(this.player));
 
         this.lighting = new Lighting(this.walls);
     }
@@ -34,35 +34,37 @@ class Game extends Basegame {
         this.intersections = this.lighting.drawLight(this.player.position);
         this.player.update();
         this.player.inventory.update();
-        this.player.inventory.content[this.player.inventory.selected].update();
     }
 
-    draw() {
+    draw(){
         Gamestate.context.fillStyle = "black";
         Gamestate.context.fillRect(0, 0, Gamestate.canvas.width, Gamestate.canvas.height);
-
+        
         Gamestate.context.fillStyle = "white";
         Gamestate.context.beginPath();
-        for (let i = 1; i < this.intersections.length; i++) {
+        for(let i = 1 ; i < this.intersections.length ; i ++){
             Gamestate.context.lineTo(this.intersections[i].x, this.intersections[i].y)
         }
-        Gamestate.context.fill();
-        this.lighting.drawLight(this.player.position);
+        Gamestate.context.fill()
+        this.lighting.drawLight(this.player.position)
 
         Gamestate.context.strokeStyle = "red"
         Gamestate.context.lineWidth = 3;
-        for (let i = 0; i < this.walls.length; i++) {
+        for(let i = 0 ; i < this.walls.length ; i ++){        
             this.walls[i].draw();
         }
 
         this.player.draw();
         this.player.inventory.draw();
-        this.player.inventory.content[this.player.inventory.selected].draw();
 
     }
 
-    mouseup() {
-        this.player.inventory.content[this.player.inventory.selected].shoot(this.player.position, Gamestate.mousePosition);
+    mousedown() {
+        this.player.startShooting();
+    }
+
+    mouseup(){
+        this.player.stopShooting();
     }
 }
 
