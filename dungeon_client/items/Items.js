3eac13b43d60b67ade2552694dfdd442ffe488d1
Martin Bogdanov <imhackinbro@gmail.com>
2021-08-26 19:00:09 +0300

    Add Items and HealthPotion

---
+++ dungeon_client/items/Items.js
@@ -0,0 +1,43 @@
+const Player = require('../player/Player.js');
+
+class Item {
+    /**
+     * @param {number} maxUses 
+     * @param {number} cooldown 
+     * @param {Player} owner 
+     */
+    constructor(maxUses, cooldown, owner) {
+        this.uses = 0;
+        this.cooldown = cooldown;
+        this.owner = owner;
+    }
+
+    use() {
+        if (this.uses >= this.maxUses) return;
+        this.uses++;
+        this.effect();
+    }
+
+    effect() { }
+}
+
+class HealthPotion extends Item {
+    /** 
+     * @param {number} cooldown 
+     * @param {Player} owner 
+     * @param {number} healAmount 
+     */
+    constructor(cooldown, owner, healAmount) {
+        super(cooldown, owner);
+        this.maxUses = 1;
+        this.healAmount = healAmount;
+    }
+
+    use() {
+        super.use();
+    }
+
+    effect() {
+        this.owner.health += this.healAmount;
+    }
+}
